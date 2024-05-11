f03c0870efaea27c8d64ccd11baf9a5ac8fc26a5
Roberta <roberta.amarto@ed3.com.br>
2024-05-11 18:54:53 -0300

    feat: inicia hero section

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-      <div className="overflow-hidden">react app</div>
+      <Hero />
     </>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import { Parallax } from 'react-parallax';
+import Image from '../assets/img/hero/menina.svg';
+import Header from './Header';
 
-const Hero = () => {
-  return <div>Hero</div>;
-};
+function Hero() {
+  return (
+    <section className="min-h-[618px] lg:min-h-[815px] pt-9 ">
+      <div className="container mx-auto flex flex-col items-start">
+        <Header />
+        <div>
+          <div className="intro flex flex-col items-start text-left">
+            <h1 className="h1 mb-3 lg:mb-6">
+              Yoga to <br /> Release Stress
+            </h1>
+            <p className="mb-6 lg:mb-12 max-w-md lg:text-lg">
+              By sharing our practice with you we hope to promote{' '}
+              <strong>self-care</strong> and
+              <strong>well-being</strong>. Learn more about yoga and become more
+              mindful and compassionate.
+            </p>
+            <div>
+              <div className="btn-group mb-12 space-x-4">
+                <button className="btn btn-sm lg:btn-lg btn-orange">
+                  Get started
+                </button>
+                <button className="btn btn-sm lg:btn-lg text-heading bg-transparent border border-stroke-1 px-4 hover:bg-stroke-3 transition">
+                  Learn more
+                </button>
+              </div>
+            </div>
+            <div>
+              <Parallax
+                className="w-full h-full p-28 lg:p-16"
+                bgImage={Image}
+                bgImageAlt="yoga girl"
+                strength={200}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
 
 export default Hero;
