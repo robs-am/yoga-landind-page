a525b19ab91801fb1a5e7a145d81d6b67c5dd08a
Roberta <roberta.amarto@ed3.com.br>
2024-05-11 16:31:52 -0300

    feat: altera arquvios tsx --> jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+// import components
+import Hero from './components/Hero';
+import Cards from './components/Cards';
+import Facts from './components/Facts';
+import Features from './components/Features';
+import Courses from './components/Courses';
+import Pricing from './components/Pricing';
+import Newsletter from './components/Newsletter';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <div className="overflow-hidden">react app</div>
+    </>
+  );
+}
+
+export default App;
