0cbaebc93d40f3a2d0ea9d84ab1817ede7c84217
Roberta <roberta.amarto@ed3.com.br>
2024-05-14 23:10:18 -0300

    feat: altera h1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Parallax } from 'react-parallax';
+import { hero } from '../data';
 
 import Header from './Header';
 
@@ -11,13 +12,8 @@ function Hero() {
           <Header />
           <div className="flex flex-col mt-28 lg:flex-row">
             <div className="intro flex flex-col items-start justify-center text-left max-w-80 lg:max-w-none">
-              <h1 className="h1  mb-3 lg:mb-6">
-                Yoga to <br /> Release Stress
-              </h1>
-              <p className="mb-6 lg:mb-12 max-w-md lg:text-lg">
-                Explore the Healing Path of Yoga, Suitable for All Ages and
-                Levels
-              </p>
+              <h1 className="h1  mb-3 lg:mb-6">{hero.title}</h1>
+              <p className="mb-6 lg:mb-12 max-w-md lg:text-lg">{hero.desc}</p>
               <div>
                 <div className="btn-group mb-8 space-x-4">
                   <button className="btn btn-sm lg:btn-lg text-white bg-gray hover:bg-gray-900 transition">
@@ -33,14 +29,7 @@ function Hero() {
           {/* image */}
           <div className="lg:w-auto">
             <div className="flex-1 flex justify-center lg:justify-end">
-              <div className="h-[234px] lg:w-[504px] lg:h-[744px] lg:mt-0">
-                {/* <Parallax
-                  className="w-full h-full p-28 lg:p-16"
-                  bgImage={Image}
-                  bgImageAlt="yoga girl"
-                  strength={150}
-                /> */}
-              </div>
+              <div className="h-[234px] lg:w-[504px] lg:h-[744px] lg:mt-0"></div>
             </div>
           </div>
         </div>
--- src/data.jsx
+++ src/data.jsx
@@ -7,8 +7,8 @@ import CourseImage2 from '../src/assets/img/courses/course-2.png';
 import CourseImage3 from '../src/assets/img/courses/course-3.png';
 
 export const hero = {
-  title: 'Yoga to\n Release Stress',
-  desc: 'By sharing our practice with you we hope to promote self-care and well-being. Learn more about yoga and become more mindful and compassionate.',
+  title: 'Flow into\nWellness',
+  desc: 'By sharing our practice with you we hope to promote self-care and well-being.\n\nLearn more about yoga and become more mindful and compassionate.',
 };
 
 export const navigation = [
